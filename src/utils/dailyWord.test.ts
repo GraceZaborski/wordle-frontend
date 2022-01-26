f27507a75c2fd85564d85451eacefe9b2948798d
Grace Zaborski <grace.zaborski@beamery.com>
2022-01-26 18:34:18 +0000

    utils function test

---
+++ src/utils/dailyWord.test.ts
@@ -0,0 +1,21 @@
+import { dailyWord } from './dailyWord'
+
+const wordList = ["hello", "magic", "money", "talks", "walks"]
+
+test("Function is called once", () => {
+    expect(
+        dailyWord.length
+    ).toEqual(1)
+})
+
+test("5-letter word returned", () => {
+    expect(
+        dailyWord(wordList).length
+    ).toEqual(5)
+})
+
+// test("The random word returned is a string", () => {
+//     expect(
+//         typeof dailyWord
+//     ).toBe("string")
+// })
--- src/utils/dailyWord.ts
+++ src/utils/dailyWord.ts
@@ -1,8 +1,6 @@
 import { wordList } from '.././data/data'
 
-function dailyWord(wordList: string[]) {
+export function dailyWord(wordList: string[]) {
     const dailyWord = wordList[Math.floor(Math.random() * wordList.length)]
     return dailyWord
 }
-
-console.log(dailyWord)
